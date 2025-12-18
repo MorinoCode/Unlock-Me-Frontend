2c1328bdfccab3104b0f690c0b07a16a0138107f
MorinoCode <mortezamastercode@gmail.com>
2025-12-18 12:00:01 +0100

    basic signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignupPage from './pages/signupPage/SignupPage'
 
 const App = () => {
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route   />
+        <Route path='/signup' element={<SignupPage />}  />
       </Routes>
     </BrowserRouter>
     </>
