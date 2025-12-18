9d2848eb74a307cae26fae847729b35c2f0b40ff
MorinoCode <mortezamastercode@gmail.com>
2025-12-18 22:33:44 +0100

    add Homepage style

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SignupPage from './pages/signupPage/SignupPage'
 import Login from './pages/signinPage/SigninPage'
 import InitialQuizzesPage from './pages/initialQuizzesPage/InitialQuizzesPage'
+import HomePage from './pages/homePage/HomePage'
 
 const App = () => {
   return (
     <>
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<HomePage />}  />
         <Route path='/signup' element={<SignupPage />}  />
         <Route path='/signin'  element={<Login />}  />
         <Route path='/initial-quizzes'  element={<InitialQuizzesPage />}  />
