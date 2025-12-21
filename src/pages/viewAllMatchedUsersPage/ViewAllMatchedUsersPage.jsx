d010b6343982d302f3d12933c66321a678f93b4e
MorinoCode <mortezamastercode@gmail.com>
2025-12-21 16:57:14 +0100

    fix import

--- src/pages/viewAllMatchedUsersPage/ViewAllMatchedUsersPage.jsx
+++ src/pages/viewAllMatchedUsersPage/ViewAllMatchedUsersPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import UserCard from "../../components/UserCard/UserCard";
+import UserCard from "../../components/UserCard/UserCard.jsx";
 import "./ViewAllMatchedUsersPage.css";
 
 const ViewAllMatchedUsersPage = () => {
